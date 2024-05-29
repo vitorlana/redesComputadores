8d6afd2bd89a5860bd452e1074086c698592f334
Vitor Lana <vitorhugolana@hotmail.com>
2024-05-29 17:25:00 -0300

    finish

--- TP2-2024/cod/client.c
+++ TP2-2024/cod/client.c
@@ -9,7 +9,16 @@
 
 #define BUFFER_SIZE 500
 
+int se_client_id = -1;
+int scii_client_id = -1;
+
 void communicate_with_servers(int se_socket, int scii_socket);
+void removeNewline(char *str);
+void send_req_status(int se_socket);
+void handle_res_status(char *response, int scii_socket);
+void handle_res_up(char *response);
+void handle_res_none(char *response);
+void handle_res_down(char *response);
 
 int main(int argc, char *argv[]) {
     if (argc != 4) {
@@ -95,11 +104,27 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+void removeNewline(char *str) {
+    char *src, *dst;
+    src = dst = str;
+    
+    while (*src) {
+        if (*src != '\n') {
+            *dst++ = *src;
+        }
+        src++;
+    }
+    *dst = '\0';
+}
+
 void communicate_with_servers(int se_socket, int scii_socket) {
-    char buffer[BUFFER_SIZE];
+    char bufferenv[BUFFER_SIZE];
+    char buffersce[BUFFER_SIZE];
+    char bufferscii[BUFFER_SIZE];
+
     fd_set read_fds;
     int max_fd = se_socket > scii_socket ? se_socket : scii_socket;
-    
+
     while (1) {
         FD_ZERO(&read_fds);
         FD_SET(STDIN_FILENO, &read_fds);
@@ -113,40 +138,157 @@ void communicate_with_servers(int se_socket, int scii_socket) {
 
         if (FD_ISSET(STDIN_FILENO, &read_fds)) {
             // Leitura de entrada do usuário
-            if (fgets(buffer, BUFFER_SIZE, stdin) == NULL) {
+            memset(bufferenv, 0, BUFFER_SIZE); // Limpa o buffer antes de ler a entrada do usuário
+
+            if (fgets(bufferenv, BUFFER_SIZE, stdin) == NULL) {
                 perror("fgets");
                 exit(EXIT_FAILURE);
+            }else if (strncmp(bufferenv, "kill", 4) == 0) {
+                if (se_client_id != -1) {
+                    snprintf(bufferenv, BUFFER_SIZE, "REQ_REM(%d)", se_client_id);
+                    send(se_socket, bufferenv, strlen(bufferenv), 0);
+                }
+                if (scii_client_id != -1) {
+                    snprintf(bufferenv, BUFFER_SIZE, "REQ_REM(%d)", scii_client_id);
+                    send(scii_socket, bufferenv, strlen(bufferenv), 0);
+                }
+            }else if (strncmp(bufferenv, "display info se", 15) == 0) {
+                    snprintf(bufferenv, BUFFER_SIZE, "REQ_INFOSE");
+                    send(se_socket, bufferenv, strlen(bufferenv), 0);
+            }else if (strncmp(bufferenv, "display info scii", 17) == 0) {
+                    snprintf(bufferenv, BUFFER_SIZE, "REQ_INFOSCII");
+                    send(scii_socket, bufferenv, strlen(bufferenv), 0);
+            }else if (strncmp(bufferenv, "query condition", 15) == 0) {
+                    snprintf(bufferenv, BUFFER_SIZE, "REQ_STATUS");
+                    send(se_socket, bufferenv, strlen(bufferenv), 0);
+            }else{
+                printf("Comando inválido: %s\n", bufferenv);
             }
-
-            // Verifica se a mensagem é "kill"
-            if (strncmp(buffer, "kill", 4) == 0) {
-                printf("Encerrando conexões...\n");
-                send(se_socket, buffer, strlen(buffer), 0);
-                send(scii_socket, buffer, strlen(buffer), 0);
-                break;
-            }
-
-            // Envia a mensagem para ambos os servidores
-            send(se_socket, buffer, strlen(buffer), 0);
-            send(scii_socket, buffer, strlen(buffer), 0);
+            memset(bufferenv, 0, BUFFER_SIZE); // Limpa o buffer após enviar a mensagem
         }
 
         if (FD_ISSET(se_socket, &read_fds)) {
             // Recebe a mensagem do servidor SE
-            int bytes_received = recv(se_socket, buffer, BUFFER_SIZE, 0);
+            memset(buffersce, 0, BUFFER_SIZE); // Limpa o buffer antes de receber a mensagem
+            int bytes_received = recv(se_socket, buffersce, BUFFER_SIZE, 0);
+            // printf("From SCE: %s\n", buffersce);
+
+
             if (bytes_received > 0) {
-                buffer[bytes_received] = '\0';
-                printf("From SE: %s\n", buffer);
+                buffersce[bytes_received] = '\0';
+                if (strncmp(buffersce, "ERROR(01)",9) == 0) {
+                    printf("Client limit exceeded\n");
+                    close(se_socket);
+                    close(scii_socket);
+                    exit(EXIT_FAILURE);
+                } else if (strncmp(buffersce, "RES_ADD(", 8) == 0) {
+                    sscanf(buffersce, "RES_ADD(%d)", &se_client_id);
+                    printf("Servidor SE New ID: %d\n", se_client_id);
+                } else if (strncmp(buffersce, "OK(01)", 6) == 0) {
+                    printf("Successful disconnect\n");
+                    close(se_socket);
+                    se_client_id = -1;
+                    if (scii_client_id == -1) {
+                        exit(EXIT_SUCCESS);
+                    }
+                } else if (strncmp(buffersce, "ERROR(02)", 9) == 0) {
+                    printf("Client not found\n");
+                } else if (strncmp(buffersce, "RES_INFOSE(", 11) == 0) {
+                    int prod_energy;
+                    sscanf(buffersce, "RES_INFOSE(%d)", &prod_energy);
+                    printf("producao atual: %d kWh\n", prod_energy);
+                } else if (strncmp(buffersce, "RES_STATUS", 10) == 0) {
+                    handle_res_status(buffersce, scii_socket);
+                } else {
+                    // printf("From SE: %s\n", buffer);
+                }
             }
+            memset(buffersce, 0, BUFFER_SIZE); // Limpa o buffer após processar a mensagem recebida
         }
 
         if (FD_ISSET(scii_socket, &read_fds)) {
             // Recebe a mensagem do servidor SCII
-            int bytes_received = recv(scii_socket, buffer, BUFFER_SIZE, 0);
+            memset(bufferscii, 0, BUFFER_SIZE); // Limpa o buffer antes de receber a mensagem
+
+            int bytes_received = recv(scii_socket, bufferscii, BUFFER_SIZE, 0);
+            // printf("From SCII: %s\n", bufferscii);
+
             if (bytes_received > 0) {
-                buffer[bytes_received] = '\0';
-                printf("From SCII: %s\n", buffer);
+                bufferscii[bytes_received] = '\0';
+                if (strncmp(bufferscii, "ERROR(01)", 9) == 0) {
+                    printf("Client limit exceeded\n");
+                    close(se_socket);
+                    close(scii_socket);
+                    exit(EXIT_FAILURE);
+                } else if (strncmp(bufferscii, "RES_ADD(", 8) == 0) {
+                    sscanf(bufferscii, "RES_ADD(%d)", &scii_client_id);
+                    printf("Servidor SCII New ID: %d\n", scii_client_id);
+                } else if (strncmp(bufferscii, "OK(01)", 6) == 0) {
+                    printf("Successful disconnect\n");
+                    close(scii_socket);
+                    scii_client_id = -1;
+                    if (se_client_id == -1) {
+                        exit(EXIT_SUCCESS);
+                    }
+                } else if (strncmp(bufferscii, "ERROR(02)", 9) == 0) {
+                    printf("Client not found\n");
+                }else if (strncmp(bufferscii, "RES_INFOSCII(", 12) == 0) {
+                    int cons_energy;
+                    sscanf(bufferscii, "RES_INFOSCII(%d)", &cons_energy);
+                    printf("consumo atual: %d %%\n", cons_energy);
+                } else if (strncmp(bufferscii, "RES_UP", 6) == 0) {
+                    handle_res_up(bufferscii);
+                } else if (strncmp(bufferscii, "RES_NONE", 8) == 0) {
+                    handle_res_none(bufferscii);
+                } else if (strncmp(bufferscii, "RES_DOWN", 8) == 0) {
+                    handle_res_down(bufferscii);
+                } else {
+                    // printf("From SCII: %s\n", bufferscii);
+                }
             }
+            memset(bufferscii, 0, BUFFER_SIZE); // Limpa o buffer após processar a mensagem recebida
         }
     }
+}
+
+void send_req_status(int se_socket) {
+    char message[] = "REQ_STATUS";
+    send(se_socket, message, strlen(message), 0);
+}
+
+void handle_res_status(char *response, int scii_socket) {
+    char estado[BUFFER_SIZE];
+    sscanf(response, "RES_STATUS(%[^)])", estado);
+    printf("estado atual: %s\n", estado);
+
+    if (strcmp(estado, "alta") == 0) {
+        char message[] = "REQ_UP";
+        send(scii_socket, message, strlen(message), 0);
+    } else if (strcmp(estado, "moderada") == 0) {
+        char message[] = "REQ_NONE";
+        send(scii_socket, message, strlen(message), 0);
+    } else if (strcmp(estado, "baixa") == 0) {
+        char message[] = "REQ_DOWN";
+        send(scii_socket, message, strlen(message), 0);
+    }
+}
+
+void handle_res_up(char *response) {
+    int old_value, new_value;
+    sscanf(response, "RES_UP(%d,%d)", &old_value, &new_value);
+    printf("consumo antigo: %d %%\n", old_value);
+    printf("consumo atual: %d %%\n", new_value);
+}
+
+void handle_res_none(char *response) {
+    int current_value;
+    sscanf(response, "RES_NONE(%d)", &current_value);
+    printf("consumo atual: %d %%\n", current_value);
+}
+
+void handle_res_down(char *response) {
+    int old_value, new_value;
+    sscanf(response, "RES_DOWN(%d,%d)", &old_value, &new_value);
+    printf("consumo antigo: %d %%\n", old_value);
+    printf("consumo atual: %d %%\n", new_value);
 }
---
+++ TP2-2024/scripts/run_ipv4.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+# Navegar para a pasta cod e compilar os programas
+cd ../cod
+make
+
+# Navegar de volta para a pasta scripts
+cd ../scripts
+
+# Executar o servidor em terminais separados
+osascript -e 'tell application "Terminal" to do script "cd '$(pwd)'/../cod; ./server v4 12345; exec zsh"' &
+osascript -e 'tell application "Terminal" to do script "cd '$(pwd)'/../cod; ./server v4 54321; exec zsh"' &
+
+# Aguardar para garantir que os servidores estejam prontos
+sleep 10
+
+# Executar cada cliente em terminais separados usando redirecionamento de input e mantendo o terminal aberto
+osascript -e 'tell application "Terminal" to do script "cd '$(pwd)'; ./../cod/client 127.0.0.1 12345 54321; exec zsh"' &
+osascript -e 'tell application "Terminal" to do script "cd '$(pwd)'; ./../cod/client 127.0.0.1 12345 54321; exec zsh"' &
+osascript -e 'tell application "Terminal" to do script "cd '$(pwd)'; ./../cod/client 127.0.0.1 12345 54321; exec zsh"' &
+osascript -e 'tell application "Terminal" to do script "cd '$(pwd)'; ./../cod/client 127.0.0.1 12345 54321; exec zsh"' &
+osascript -e 'tell application "Terminal" to do script "cd '$(pwd)'; ./../cod/client 127.0.0.1 12345 54321; exec zsh"' &
+osascript -e 'tell application "Terminal" to do script "cd '$(pwd)'; ./../cod/client 127.0.0.1 12345 54321; exec zsh"' &
+osascript -e 'tell application "Terminal" to do script "cd '$(pwd)'; ./../cod/client 127.0.0.1 12345 54321; exec zsh"' &
+osascript -e 'tell application "Terminal" to do script "cd '$(pwd)'; ./../cod/client 127.0.0.1 12345 54321; exec zsh"' &
+osascript -e 'tell application "Terminal" to do script "cd '$(pwd)'; ./../cod/client 127.0.0.1 12345 54321; exec zsh"' &
+osascript -e 'tell application "Terminal" to do script "cd '$(pwd)'; ./../cod/client 127.0.0.1 12345 54321; exec zsh"' &
+osascript -e 'tell application "Terminal" to do script "cd '$(pwd)'; ./../cod/client 127.0.0.1 12345 54321; exec zsh"' &
+
+
